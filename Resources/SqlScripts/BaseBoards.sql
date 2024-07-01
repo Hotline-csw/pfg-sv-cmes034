563a1d537aa1bf7b821c59b2a4855873cfbe3a06
sguhl <sys@homag-group.com>
2024-07-01 06:45:05 +0000

    Modified#Resources\SqlScripts\BaseBoards.sql

--- Resources/SqlScripts/BaseBoards.sql
+++ Resources/SqlScripts/BaseBoards.sql
@@ -1,10 +1,19 @@
--- CREATING NEW TABLE
--- The schema name has to be 'cust'
-CREATE TABLE[cust].[BaseBoards]
- ([Sequence] [bigint] IDENTITY(1,1) NOT NULL,
-[CreationDate] [datetime2](7) NOT NULL default getdate(),
-[CreationSource] [nvarchar](32) NULL,
-[ModificationDate] [datetime2](7) NOT NULL default getdate(),
-[ModificationSource] [nvarchar](32) NULL,
-[Locked] [bit]NOT NULL default 0,
-[LockSource] [nvarchar](32) NULL)
+-- intelliStack base boards (Schonerplatten)
+CREATE TABLE [cust].[BaseBoards]
+(
+    Sequence                        bigint          not null identity(1,1),
+    BaseBoardCode                   nvarchar(32)    not null,                   -- Name der Schonplatte
+                                                                                -- z.B. SP1, SP2
+    Description                     nvarchar(256)   null,                       -- Beschreibung
+    Length                          numeric(18,2)   not null,                   -- Länge der Schonplatte
+    Width                           numeric(18,2)   not null,                   -- Breite der Schonplatte
+    Thickness                       numeric(18,2)   not null,                   -- Dicke der Schonplatte
+    CreationDate                    datetime2       not null default getdate(),
+    CreationSource                  nvarchar(32)    null,
+    ModificationDate                datetime2       not null default getdate(),
+    ModificationSource              nvarchar(32)    null,
+    Locked                          bit             not null default 0,
+    LockSource                      nvarchar(32)    null,
+
+    CONSTRAINT [PK_BaseBoards] PRIMARY KEY ( BaseBoardCode )
+)
