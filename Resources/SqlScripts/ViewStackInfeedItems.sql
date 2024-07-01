cdd418f222abd137f3f569b68fb061ff2b495e5b
sguhl <sys@homag-group.com>
2024-07-01 06:47:34 +0000

    Added#Resources\SqlScripts\ViewStackInfeedItems.sql

---
+++ Resources/SqlScripts/ViewStackInfeedItems.sql
@@ -0,0 +1,6 @@
+-- CREATING NEW VIEW
+-- The schema name has to be 'cust'
+-- The view name has to start with the prefix 'View'
+CREATE VIEW[cust].[ViewStackInfeedItems]
+ AS SELECT DISTINCT base.Users.Sequence
+FROM base.Users
