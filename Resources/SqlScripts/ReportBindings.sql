1683d788e95c658cee9295aad438851dc1eae79a
sfeist <sys@homag-group.com>
2025-06-13 14:23:50 +0000

    Modified#Resources\SqlScripts\ReportBindings.sql

--- Resources/SqlScripts/ReportBindings.sql
+++ Resources/SqlScripts/ReportBindings.sql
@@ -4,15 +4,12 @@
 CREATE TABLE[cust].[ReportBindings]
 (
     Sequence                        bigint          not null identity(1,1),
-    DateReportRelevance             datetime2       not null,                   -- Datum für das der Report relevant ist
-    PlanningGroup                   nvarchar(32)    not null,                   -- Dispogruppe
     WorkOrder                       nvarchar(32)    not null,                   -- Werkauftrag
     ProductionOrderCode             nvarchar(32)    null,                       -- Fertigungsauftrag
     ProductionItemCode              nvarchar(32)    null,                       -- Bauteilnummer
     CustomerOrderCode               nvarchar(32)    null,                       -- Kundenauftragsnummer
     CustomerOrderPosition           nvarchar(32)    null,                       -- Kundenauftragsposition
     OptimizationCode                nvarchar(32)    null,                       -- Optimierungsnummer
-    VirtualCartCode                 nvarchar(64)    null,                       -- Virtuelle Wagennummer
     ReportLayout                    nvarchar(64)    not null,                   -- Name des Report Layout
     ReportEntity                    nvarchar(64)    not null,                   -- Tabelle, an der der Report logisch zugeordnet ist
     ReportSequence                  bigint          not null,                   -- Sequence zu ReportEntity
