3d0509d2e3d4635095bfe1236bcfe8757a978a9b
sfeist <sys@homag-group.com>
2025-06-13 14:54:43 +0000

    Modified#Resources\SqlScripts\ViewReportBindings.sql

--- Resources/SqlScripts/ViewReportBindings.sql
+++ Resources/SqlScripts/ViewReportBindings.sql
@@ -1,6 +1,24 @@
--- CREATING NEW VIEW
--- The schema name has to be 'cust'
--- The view name has to start with the prefix 'View'
+-- Printing Center Confi
+-- SF - 2025-06-13
+
 CREATE VIEW[cust].[ViewReportBindings]
- AS SELECT DISTINCT base.Users.Sequence
-FROM base.Users
+AS
+SELECT rb.Sequence,
+       rb.WorkOrder,
+       rb.ProductionOrderCode,
+       rb.ProductionItemCode,
+       rb.CustomerOrderCode,
+       rb.CustomerOrderPosition,
+       rb.OptimizationCode,
+       rb.ReportLayout,
+       r.Description AS ReportDescription,
+       CASE ISNULL( rb.BinariesSequence, 0 )
+            WHEN 0
+            THEN 0
+            ELSE 1
+        END AS ReportIsFinished,
+       rb.ReportEntity,
+       rb.ReportSequence,
+       rb.BinariesSequence
+FROM cust.ReportBindings rb
+JOIN cust.Reports r ON rb.ReportLayout = r.Layout
