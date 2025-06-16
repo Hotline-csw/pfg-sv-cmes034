b8b6e84ff7775fca4c7f69410d192dd2692b2bc2
sfeist <sys@homag-group.com>
2025-06-16 13:40:40 +0000

    Modified#Resources\SqlScripts\PrinterDefaults.sql

--- Resources/SqlScripts/PrinterDefaults.sql
+++ Resources/SqlScripts/PrinterDefaults.sql
@@ -1,10 +1,25 @@
--- CREATING NEW TABLE
--- The schema name has to be 'cust'
-CREATE TABLE[cust].[PrinterDefaults]
- ([Sequence] [bigint] IDENTITY(1,1) NOT NULL,
-[CreationDate] [datetime2](7) NOT NULL default getdate(),
-[CreationSource] [nvarchar](32) NULL,
-[ModificationDate] [datetime2](7) NOT NULL default getdate(),
-[ModificationSource] [nvarchar](32) NULL,
-[Locked] [bit]NOT NULL default 0,
-[LockSource] [nvarchar](32) NULL)
+-- Printing Center Confi
+-- SF - 2025-06-16
+
+CREATE TABLE [cust].[PrinterDefaults]
+(
+    Sequence                        bigint          not null identity(1,1),
+    UserName                        nvarchar(32)    not null,                   -- Benutzername
+    ReportLayout                    nvarchar(64)    not null,                   -- Name des Report Layout
+    Dialogue                        nvarchar(64)    not null,                   -- Name der aufrufenden Maske
+    Printer                         nvarchar(64)    not null,                   -- Name des Druckers
+    IsDefault                       int             not null default 0,                   -- Defaultkennzeichen
+                                                                                --   0 = kein Default
+                                                                                --   1 = Printer ist Default für diesen Benutzer,
+                                                                                --       diesen Report und diese Maske
+    Description                     nvarchar(256)   null,                       -- Beschreibung
+    CreationDate                    datetime2       not null default getdate(),
+    CreationSource                  nvarchar(32)    null,
+    ModificationDate                datetime2       not null default getdate(),
+    ModificationSource              nvarchar(32)    null,
+    Locked                          bit             not null default 0,
+    LockSource                      nvarchar(32)    null,
+
+    CONSTRAINT [PK_PrinterDefaults] PRIMARY KEY ( UserName, ReportLayout, Dialogue, Printer ),
+    CONSTRAINT [FK_PrinterDefaults_Printers] FOREIGN KEY ( Printer ) REFERENCES [cust].[Printers] ( Printer ) ON DELETE CASCADE
+)
