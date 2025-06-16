b5e1c2343bb15aa0efc8a3e0f3c552e9178f1de1
sfeist <sys@homag-group.com>
2025-06-16 14:00:17 +0000

    Modified#Resources\SqlScripts\Printers.sql

--- Resources/SqlScripts/Printers.sql
+++ Resources/SqlScripts/Printers.sql
@@ -1,10 +1,21 @@
--- CREATING NEW TABLE
--- The schema name has to be 'cust'
+-- Printing Center Confi
+-- SF - 2025-06-16
+
 CREATE TABLE[cust].[Printers]
- ([Sequence] [bigint] IDENTITY(1,1) NOT NULL,
-[CreationDate] [datetime2](7) NOT NULL default getdate(),
-[CreationSource] [nvarchar](32) NULL,
-[ModificationDate] [datetime2](7) NOT NULL default getdate(),
-[ModificationSource] [nvarchar](32) NULL,
-[Locked] [bit]NOT NULL default 0,
-[LockSource] [nvarchar](32) NULL)
+(
+    Sequence                        bigint          not null identity(1,1),
+    Printer                         nvarchar(64)    not null,                   -- Name des Druckers
+    PrinterType                     int             not null default 0,         -- Typisierung des Druckers
+                                                                                --   0 = nicht bekannt
+                                                                                --   1 = DIN A4
+                                                                                --   2 = Etikett
+    Description                     nvarchar(256)   null,                       -- Beschreibung
+    CreationDate                    datetime2       not null default getdate(),
+    CreationSource                  nvarchar(32)    null,
+    ModificationDate                datetime2       not null default getdate(),
+    ModificationSource              nvarchar(32)    null,
+    Locked                          bit             not null default 0,
+    LockSource                      nvarchar(32)    null,
+
+    CONSTRAINT [PK_Printers] PRIMARY KEY ( Printer )
+)
