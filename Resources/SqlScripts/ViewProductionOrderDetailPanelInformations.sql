a9f5a8ff5dff8591017b2648219bb40c4e48a2e8
sfeist <sys@homag-group.com>
2026-02-10 09:30:53 +0000

    Modified#Resources\SqlScripts\ViewProductionOrderDetailPanelInformations.sql

--- Resources/SqlScripts/ViewProductionOrderDetailPanelInformations.sql
+++ Resources/SqlScripts/ViewProductionOrderDetailPanelInformations.sql
@@ -1,6 +1,43 @@
--- CREATING NEW VIEW
--- The schema name has to be 'cust'
--- The view name has to start with the prefix 'View'
+-- Zusatzinformationen in der Fertigungsauftragsmaske
+-- created by S.Feist / 2026-02-10
+
 CREATE VIEW[cust].[ViewProductionOrderDetailPanelInformations]
- AS SELECT DISTINCT base.Users.Sequence
-FROM base.Users
+ AS 
+ SELECT 
+	-- Allgemeine Informationen
+	po.[Code] As ProductionOrderCode
+	,po.[CustomerOrderCode]
+	,po.[CustomerOrderPosition]
+	,po.[ArticleNumber]
+	,po.[ArticleDescription]
+	,po.[DesiredTargetQuantity]
+	,po.[Material]
+	,po.[MaterialCategory]
+	,po.[Grain]
+	
+	-- Dimensionen
+	,po.[Length]
+	,po.[CuttingLength]
+	,po.[Width]
+	,po.[CuttingWidth]
+	,po.[Thickness]
+	,po.[CuttingThickness]
+
+	-- Bekantung
+	,po.[EdgeTransition]
+	,po.[EdgeShape]
+
+	-- Fertigungsweg
+	,po.[ComponentType]
+	,po.[RouteCode]
+	,po.[ProductionRoute]
+
+	-- Nachfertigung
+	,po.[ReproductionType]
+	,po.[OriginalProductionOrderCode]
+	,po.[OriginalProductionItemCode]
+	
+	-- Sonstige Informationen (cust-Felder)
+	,po.[CustomProductionType]
+
+from base.ProductionOrders po
